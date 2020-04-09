018fea8e2bcb7f4086683e59d0ab9d6d2c655dee
txu1 <txu1@i9i8.com>
2020-04-09 17:34:10 +0800

    add selection sort

---
+++ src/main/java/com/monkey/sort/SelectionSort.java
@@ -0,0 +1,34 @@
+package com.monkey.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort implements IArraySort {
+    /**
+     * 选择排序
+     *
+     * @param sourceArray 待排序数组
+     * @return 排序后数组
+     */
+    @Override
+    public int[] sort(int[] sourceArray) {
+        int[] array = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int tmp;
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                // 交换位置
+                tmp = array[i];
+                array[i] = array[min];
+                array[min] = tmp;
+            }
+        }
+        return array;
+    }
+}
--- src/test/java/com/monkey/sort/TestSort.java
+++ src/test/java/com/monkey/sort/TestSort.java
@@ -1,5 +1,7 @@
 package com.monkey.sort;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -9,11 +11,26 @@ public class TestSort {
     //private int[] sourceArray = {20, 1, 8, 9, 3, 4, 17, 12, 11, 2, 7, 15, 19};
     private int[] sourceArray = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-    @Test
-    public void testBubbleSort() {
-        IArraySort arraySort = new BubbleSort();
+    private IArraySort arraySort;
+
+    @Before
+    public void beforeTest() {
         System.out.println(Arrays.toString(sourceArray));
+    }
+
+    @After
+    public void afterTest() {
         int[] targetArray = arraySort.sort(sourceArray);
         System.out.println(Arrays.toString(targetArray));
+    }
+
+    @Test
+    public void testBubbleSort() {
+        arraySort = new BubbleSort();
+    }
+
+    @Test
+    public void testSelectionSort() {
+        arraySort = new SelectionSort();
     }
 }
